3e5e0fba42b4ad5a3db903a85caae6284fb41b81
Steven Kirwan <Steven.Kirwan@strata3.com>
2022-01-20 10:26:02 +0000

    Updated app layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import Theme from '../components/global/theme/Theme'
+import Layout from "../components/global/layout/Layout";
+
+export function reportWebVitals(metric) {
+  console.log(metric)
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout><Theme><Component {...pageProps} /></Theme></Layout>
 }
 
 export default MyApp
