7f22d14169b803e465fc8ada605674291f5b85fc
Steven Kirwan <Steven.Kirwan@strata3.com>
2022-01-20 11:11:16 +0000

    Updated crossorigin to non boolean

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <meta name="description" content="Countries App" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;600&display=swap"
             rel="stylesheet"
