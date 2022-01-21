7ab1dd1cccf12b1051554e42b9c79165e0504315
Steven Kirwan <Steven.Kirwan@strata3.com>
2022-01-21 12:29:31 +0000

    Update endpoint

--- utils/getBorders.js
+++ utils/getBorders.js
@@ -2,7 +2,7 @@ export const getBorders = async (codes) => {
   
       if (codes) {
         const res = await fetch(
-          `${process.env.ENDPOINT}/alpha?codes=${codes.toString()}`
+          `https://restcountries.com/v3.1/alpha?codes=${codes.toString()}`
         );
         const json = await res.json();
         return json;
--- utils/getData.js
+++ utils/getData.js
@@ -1,6 +1,6 @@
 export const getData = async (params) => {
   try {
-    const res = await fetch(`${process.env.ENDPOINT}${params}`);
+    const res = await fetch(`https://restcountries.com/v3.1/${params}`);
     const data = await res.json();
     return data;
   } catch (error) {
